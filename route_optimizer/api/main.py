af34e4e17bef8c3fba1e38421b2498f2d016a17b
Dhairya Rohilla <rohilladhairya139@gmail.com>
2026-06-19 15:59:54 +0530

    Fix CORS for Vercel deployment

--- route_optimizer/api/main.py
+++ route_optimizer/api/main.py
@@ -50,7 +50,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://logi-dispatch-gfkvrkrot-dhairyaxs-projects.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
